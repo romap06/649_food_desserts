70f273b0a19460c28135212904377e0bf99f6bf7
romap06 <80007541+romap06@users.noreply.github.com>
2022-04-12 14:22:00 -0400

    Update final_streamlit_pt1.py

--- final_streamlit_pt1.py
+++ final_streamlit_pt1.py
@@ -40,7 +40,7 @@ scatter_plot = alt.Chart(final_state_level
 ).mark_point(filled=True, stroke="white", strokeWidth=0.5).encode(
     x=alt.X("MedianIncome:Q", scale=alt.Scale(domain=[35000, 115000]), axis=alt.Axis(title="Median Income", gridOpacity=0.1)),
     y=alt.Y("Walk:Q", axis=alt.Axis(gridOpacity=0.1)),
-    size=alt.Size("TotalPop:Q", legend=alt.Legend(title="Total Population")),
+    size=alt.Size("TotalPop:Q", legend=alt.Legend(title="Total Population", symbolFillColor = "gray")),
     color=alt.Color("food_desert_label:N", legend=alt.Legend(title="Food Desert Label")),
     tooltip = ["State:N", "MedianIncome:Q", "FoodDesert_Totals:Q", "Region:N"],
     opacity=alt.condition(click, alt.value(1), alt.value(0.2))
